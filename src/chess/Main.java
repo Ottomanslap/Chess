463220c944df353e0a6b0fe2f49011090f3a477a
baldr <baldr@192.168.134.109>
2016-10-16 12:36:11 +0100

    Added menu & finding out how ENUMs work
    Menu asks what kind the player wants to play
    Choices are: new game; load game and puzzles. loading and puzzles aren't
    implemented yet and new game only writes the board once. But now we at
    least have a menu which is nice.

--- src/chess/Main.java
+++ src/chess/Main.java
@@ -4,51 +4,85 @@ import java.io.*;
 import java.util.Scanner;
 
 class Main {
-	
+
+	public static Mode mode = null;
+	// Trying out ENUM & track if normal game or puzzle.
+
 	public static boolean winCondition;
 	//So that we know when to stop playing
-	
+
 	private static Scanner input = new Scanner( System.in );
 	//To receive inputs from user
-	
+
 	public static Square[][] table = new Square[8][8];
 	//Creates a 2D array of squares, the size of 8*8 for the chess board
-	
+
 	public static Piece[][] tableConf = new Piece[8][8];
 	//To set up a predetermined piece configuration
-	
+
 	public static void main(String[] args) throws IOException {
-		
+
+		//Checking to see whether Symbols or Letters should be used for printing.
 		System.out.print("Can your setup handle symbols? (y/n/idk):");
-		
+
 		String answer = readInput().toLowerCase();
-		
+
 		while (!answer.equals("y")  && !answer.equals("n")){
-			
+
 			System.out.print("Can you see two different characters here? ♙♟ (y/n):");
 			answer = readInput().toLowerCase();
-			
+
 		}
-		
+
 		String SLDefault = null;
-		
+
 		if (answer.equals("y")){
 			SLDefault = "S";
 		}
 		else if (answer.equals("n")){
 			SLDefault = "L";
 		}
-		
-		init("0");
-		
-		
+		//Done Checking and configuring to match.
+
+		//Menu start
+		System.out.println("New Game  - N" + "	NOT IMPLEMENTED YET!");
+		System.out.println("Load Game - L" + "	NOT IMPLEMENTED YET!");
+		System.out.println("Puzzles   - P" + "	NOT IMPLEMENTED YET!");
+		System.out.println("Quit      - Q");
+		System.out.println("Enter choice: ");
+
+		String choice = readInput().toUpperCase();
+
+
+		while (mode == null){
+			switch (choice){
+			case "N":
+				init("0");
+				mode = Mode.CHESS;
+				break;
+			case "L":
+				//TODO: Loading
+				System.out.println("Loading is not done yet! Come back later.");
+				break;
+			case "P":
+				//TODO: Make some puzzles to be able to load them.
+				System.out.println("Puzzles are not done yet! Come back later.");
+				break;
+			case "Q":
+				return;
+			}
+
+		}
+
+		//Menu end
+
 		printTable(SLDefault);
 		//winCondition = false;
-		
+
 		//while (!winCondition){//This is where the game will take place
 		//	System.out.println("Which piece will move?");
 		//}
-		
+
 	}
 	final static void init(String board) throws IOException{
 		/*
@@ -58,21 +92,21 @@ class Main {
 		 * 
 		 * 
 		 */
-		
+
 		FileReader layout = loadMap(board);
-		
+
 		for (int r = 0 ; r < table.length ; r++){
 			for (int c = 0 ; c < table[r].length ; c++){
-				
+
 				//create the squares in the table
 				table[r][c] = new Square();
-				
+
 				//put pieces on the square
 				int symb = layout.read();
-				
+
 				while (symb == -1 || !Piece.isValid((char) symb))
 					symb = layout.read();
-				
+
 				if ((char)symb != 'x'){
 					table[r][c].piece = new Piece((char) symb);
 					//System.out.println((char)symb);
@@ -88,7 +122,7 @@ class Main {
 					table[r][c].isBlack = true;
 				}
 				//colors assigned
-				
+
 			}
 		}
 		System.out.println("Initial positions loaded.");
@@ -96,7 +130,7 @@ class Main {
 	}
 	final static void printTable(String SL){
 		if (SL != "S" && SL != "L")
-				throw new Error("ERROR: printTable recieved incorrect input for Symbol/Letter:" + SL );
+			throw new Error("ERROR: printTable recieved incorrect input for Symbol/Letter:" + SL );
 		for (int r = table.length - 1 ; r >= 0 ; r--){
 			for (int c = 0 ; c < table.length ; c++){
 				System.out.print(table[r][c].toString(SL));
@@ -105,15 +139,15 @@ class Main {
 		}
 	}
 	final static FileReader loadMap(String file) throws IOException{
-		
+
 		file = System.getProperty("java.class.path") + "//..//layouts//" + file;
 		File toBeLoaded = new File(file);
 		FileReader reader = new FileReader(toBeLoaded);
 		return reader;
-		
+
 	}
-	
-	
+
+
 	final static String readInput(){
 		return input.nextLine();
 	}
---
+++ src/chess/Mode.java
@@ -0,0 +1,5 @@
+package chess;
+
+public enum Mode {
+	CHESS, PUZZLE
+}
